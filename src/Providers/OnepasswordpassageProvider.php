07f8ede1b6149eba29c1e8035c21d637d1bc92c6
Onwuesi Williams <onwuesiwilliamsemeka@gmail.com>
2023-07-13 00:02:00 +0100

    packagist fix

--- src/Providers/OnepasswordpassageProvider.php
+++ src/Providers/OnepasswordpassageProvider.php
@@ -8,8 +8,6 @@ use Williamsonwuesi\Onepasswordpassage\Http\Middleware\PassageMiddleware;
 
 class OnepasswordpassageProvider extends ServiceProvider {
 
-
-
     /**
      * Bootstrap the application services.
      *
@@ -17,11 +15,10 @@ class OnepasswordpassageProvider extends ServiceProvider {
      */
 
     public function boot(Router $router){
-        
+
+        $this->loadMigrationsFrom(__DIR__.'/../Database/Migrations');
         $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
         $this->loadViewsFrom(__DIR__.'/../views', 'Onepasswordpassage');
-        $this->loadMigrationsFrom(__DIR__.'/../Database/Migrations');
-
         $router->middlewareGroup('passage_redirect_route', [PassageMiddleware::class]);
 
     }
